20dcf107e390ee11cb606f8c04ef36d523d339ae
Julio Gabriel <juliogabriel516@gmail.com>
2024-09-24 15:54:47 -0300

    Custom json

--- AchillesRest/App.axaml.cs
+++ AchillesRest/App.axaml.cs
@@ -1,9 +1,12 @@
+using System;
+using System.IO;
 using AchillesRest.Services;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using AchillesRest.ViewModels;
 using AchillesRest.Views;
+using Avalonia.Platform;
 using Splat;
 
 namespace AchillesRest;
@@ -17,6 +20,19 @@ public partial class App : Application
         Locator.CurrentMutable.RegisterConstant(new RequestService(), typeof(RequestService));
         Locator.CurrentMutable.Register(() => new MenuCollectionsViewModel(), typeof(MenuCollectionsViewModel));
 
+
+        // Loads an custom Json syntax highlight at AvaloniaEdit.
+        var uri = new Uri("avares://AchillesRest/Assets/Json.xshd");
+        using (var stream = AssetLoader.Open(uri))
+        {
+            using (var reader = new System.Xml.XmlTextReader(stream))
+            {
+                AvaloniaEdit.Highlighting.HighlightingManager.Instance.RegisterHighlighting("jsonachilles",
+                    new string[0],
+                    AvaloniaEdit.Highlighting.Xshd.HighlightingLoader.Load(reader,
+                        AvaloniaEdit.Highlighting.HighlightingManager.Instance));
+            }
+        }
     }
 
     public override void OnFrameworkInitializationCompleted()
